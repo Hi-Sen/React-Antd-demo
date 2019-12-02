0e5240ec00b25a7285a467eec5fa09a32a492510
HISEN\可苦可乐 <hhc_0522@163.com>
2019-12-02 09:07:24 +0800

    ~~

--- src/page/index.js
+++ src/page/index.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import { Layout, Row, Col, Avatar, Input, Menu, Dropdown, Icon, Badge  } from 'antd';
+import { Layout, Row, Col, Avatar, Input, Menu, Dropdown, Icon, Badge } from 'antd';
 import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 
 import SiderComponent from './sider'
 import Tooltip from '../component/tooltip'
 import MyNews from '../component/myNews'
 
-const { Header, Footer, Sider, Content  } = Layout;
+const { Header, Footer, Sider, Content } = Layout;
 
 // 
 const menu = (
     <Menu>
         <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="http://www.cnblogs.com/hai-cheng/">我的博客</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://blog.csdn.net/HiSen_CSDN">我的博客</a>
         </Menu.Item>
         <Menu.Item>
             <a target="_blank" rel="noopener noreferrer" href="https://github.com/Hi-Sen">我的 GitHub</a>
         </Menu.Item>
         <Menu.Item>
-            <MyNews/>
+            <MyNews />
         </Menu.Item>
         <Menu.Item>
-            <Tooltip/>
+            <Tooltip />
         </Menu.Item>
     </Menu>
 );
 
 export default class Demo1 extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props)
         this.state = {
             value: '1',
@@ -37,8 +37,8 @@ export default class Demo1 extends Component {
         }
     }
 
-    getValue (val,value) {
-        this.setState ({
+    getValue (val, value) {
+        this.setState({
             value: val,
             valueTxt: value
         })
@@ -46,18 +46,18 @@ export default class Demo1 extends Component {
 
     render () {
         return (
-            <div style={{minWidth:'1000px'}}>
+            <div style={{ minWidth: '1000px' }}>
                 <Layout>
-                    <Header style={{color:'#fff',textAlign:'center',fontWeight:'bold'}}>
+                    <Header style={{ color: '#fff', textAlign: 'center', fontWeight: 'bold' }}>
                         <Row>
                             <Col span={10}>
                                 React + Antd 实践 demo
                             </Col>
                             <Col span={8}><Input placeholder="请输入你想要的......" /></Col>
                             <Col span={6}>
-                                <Avatar style={{backgroundColor: '#87d068',marginRight:20}} icon="user" />  
+                                <Avatar style={{ backgroundColor: '#87d068', marginRight: 20 }} icon="user" />
                                 <Dropdown overlay={menu}>
-                                    <span className="ant-dropdown-link" href="#" style={{color:'#fff',cursor:'pointer'}}>
+                                    <span className="ant-dropdown-link" href="#" style={{ color: '#fff', cursor: 'pointer' }}>
                                         Hi-Sen <Icon type="down" />
                                     </span>
                                 </Dropdown>
@@ -65,21 +65,21 @@ export default class Demo1 extends Component {
                         </Row>
                     </Header>
                     <Layout>
-                        <Sider style={{color:'#666'}}>
-                            <SiderComponent 
+                        <Sider style={{ color: '#666' }}>
+                            <SiderComponent
                                 getValue={this.getValue.bind(this)}
                             />
                         </Sider>
-                        <Content style={{marginLeft:'5%',height:'86vh',paddingRight:'2%',overflow:'auto',textAlign:'center'}}>
+                        <Content style={{ marginLeft: '5%', height: '86vh', paddingRight: '2%', overflow: 'auto', textAlign: 'center' }}>
                             {this.props.children}
                         </Content>
                     </Layout>
-                    <Footer style={{backgroundColor:'#001529',position:'fixed',bottom:0,width:'100vw',minWidth:'1000px',textAlign:'center',fontWeight:'bold',color:'#fff'}}>
+                    <Footer style={{ backgroundColor: '#001529', position: 'fixed', bottom: 0, width: '100vw', minWidth: '1000px', textAlign: 'center', fontWeight: 'bold', color: '#fff' }}>
                         2018/05/31  - react + Ant Design - gitHub地址：
-                        <a href="https://github.com/Hi-Sen/React-Antd-demo-one" target="_blank">https://github.com/Hi-Sen/React-Antd-demo-one</a>     
+                        <a href="https://github.com/Hi-Sen/React-Antd-demo-one" target="_blank">https://github.com/Hi-Sen/React-Antd-demo-one</a>
                     </Footer>
                 </Layout>
-            </div> 
+            </div>
         )
     }
 }
